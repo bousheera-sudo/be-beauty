2ca412f91c498f76f0c92ee79021054ce4ec3ae8
bouchra baifouch <bouchrabaidouch@gmail.com>
2026-02-02 15:13:22 +0100

    Fix mixed content, rename Home view, remove clientapi

--- clientapi/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Be-Beauty header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Be-Beauty API Client/i);
-  expect(headerElement).toBeInTheDocument();
-});
